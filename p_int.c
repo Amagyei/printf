54805a66e6fbb169eba4a2cd90917bcd6b6318f1
amagyei <kwame.na@hotmail.com>
2023-11-14 10:32:18 +0000

    p_int

--- p_int.c
+++ p_int.c
@@ -1,6 +1,6 @@
 /* p_int - initial poit for printing int
- * 
+ *
  * Description: 'evaluate what kind of int we are dealing with and perform function
- * 
+ *
  * Return: '-1 or function call'
  */
