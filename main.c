6dd595d4a0915db57da30359b29063cb77a67990
amagyei <kwame.na@hotmail.com>
2023-11-15 13:58:37 +0000

    task 2

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%b - %b = %b\n", 2048, 1024, 1024);
-	len2 = printf("100000000000 - 10000000000 = 10000000000\n");
+	len = _printf("%b", 0);
+	len2 = printf("0");
 	fflush(stdout);
 	if (len != len2)
 	{
